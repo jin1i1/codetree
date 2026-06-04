5e9b2611e95ab1c6e2cc906bf3c0ac701de93ee6
Hye Jin Lee <133628291+jin1i1@users.noreply.github.com>
2026-06-04 16:15:44 +0900

    [Passed] 999 또는 -999 (48ms, 16MB)

---
+++ trail1/999 또는 -999/999-or-999.py
@@ -0,0 +1,15 @@
+arr = list(map(int, input().split()))
+
+min_val = arr[0]
+max_val = arr[0]
+
+for x in arr:
+    if x == 999 or x == -999:
+        break
+    
+    if x < min_val:
+        min_val = x
+    if x > max_val:
+        max_val = x
+
+print(max_val, min_val)
