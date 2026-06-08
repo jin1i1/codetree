30b19498c2d9e49da0429892c8d55b5aa87a8f53
Hye Jin Lee <133628291+jin1i1@users.noreply.github.com>
2026-06-08 15:35:33 +0900

    [Passed] 1-9 개수 세기 (49ms, 16MB)

--- trail1/1-9 개수 세기/count-one-to-nine.py
+++ trail1/1-9 개수 세기/count-one-to-nine.py
@@ -1,12 +1,9 @@
 N = int(input())
 arr = list(map(int, input().split()))
+cnt_arr = [0] * 10
+
+for num in arr:
+    cnt_arr[num] += 1
 
 for i in range(1,10):
-    cnt = 0
-    for num in arr:
-        if num == i:
-            cnt += 1
-    
-    print(cnt)
-
-
+    print(cnt_arr[i])
