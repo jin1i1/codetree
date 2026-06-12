11d483a2ce643c37a7fc8bc3da0dccb27da13270
Hye Jin Lee <133628291+jin1i1@users.noreply.github.com>
2026-06-12 23:43:53 +0900

    [Passed] 두 개의 격자 비교하기 (50ms, 16MB)

---
+++ trail1/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+arr1 = [list(map(int, input().split())) for _ in range(N)]
+arr2 = [list(map(int, input().split())) for _ in range(N)]
+arr3 = [[0] * M for _ in range(N)]
+
+for i in range(N):
+    for j in range(M):
+        if arr1[i][j] != arr2[i][j]:
+            arr3[i][j] = 1
+
+for i in range(N):
+    for j in range(M):
+        print(arr3[i][j], end = ' ')
+    print()
