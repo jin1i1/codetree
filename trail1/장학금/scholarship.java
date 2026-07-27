4cbd0182be9fc5ae47c0f017ba3f552bf9573f06
Hye Jin Lee <133628291+jin1i1@users.noreply.github.com>
2026-07-27 23:27:52 +0900

    [Passed] 장학금 (135ms, 10MB)

---
+++ trail1/장학금/scholarship.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int f = sc.nextInt();
+
+        if (m >= 90 && f >= 95) {
+            System.out.println(100000);
+        }
+        else if (m >= 90 && (f < 95 && f >= 90)) {
+            System.out.println(50000);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
