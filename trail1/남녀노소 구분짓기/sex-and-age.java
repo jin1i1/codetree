2a98e3941932077e29403d9fd83d1f07ddc5e472
Hye Jin Lee <133628291+jin1i1@users.noreply.github.com>
2026-07-28 11:34:25 +0900

    [Passed] 남녀노소 구분짓기 (124ms, 10MB)

---
+++ trail1/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int a = sc.nextInt();
+
+        if (s == 0) {
+            if (a >= 19) {
+                System.out.println("MAN");
+            }
+            else {
+                System.out.println("BOY");
+            }
+        }
+        else {
+            if (a >= 19) {
+                System.out.println("WOMAN");
+            }
+            else {
+                System.out.println("GIRL");
+            }
+        }
+    }
+}
