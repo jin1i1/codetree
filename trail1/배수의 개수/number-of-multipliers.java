b778bc52fd79e93d95219985057eaeb848bef66b
Hye Jin Lee <133628291+jin1i1@users.noreply.github.com>
2026-07-30 23:47:50 +0900

    [Passed] 배수의 개수 (131ms, 10MB)

---
+++ trail1/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+
+            if (n % 3 == 0) {
+                cnt1++;
+            }
+            if (n % 5 == 0) {
+                cnt2++;
+            }
+        }
+
+        System.out.println(cnt1 + " " + cnt2);
+    }
+}
