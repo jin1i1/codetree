6f53e01b405d346842721f25287347526dc33f73
Hye Jin Lee <133628291+jin1i1@users.noreply.github.com>
2026-09-06 23:44:16 +0900

    [Passed] 두 정수에 대한 연산값 2 (126ms, 10MB)

---
+++ trail2/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void cal(int[] arr) {
+        if (arr[0] > arr[1]) {
+            arr[0] *= 2;
+            arr[1] += 10;
+        }
+        else {
+            arr[1] *= 2;
+            arr[0] += 10;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[] arr = {a, b};
+
+        cal(arr);
+
+        System.out.print(arr[0] + " " + arr[1]);
+    }
+}
