e4fc615ad2cc5331143a08a83e268db6b97f4d81
Hye Jin Lee <133628291+jin1i1@users.noreply.github.com>
2026-09-08 23:26:46 +0900

    [Passed] 반복 출력하기 2 (128ms, 11MB)

---
+++ trail2/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void printStar(int n) {
+        if (n==00) {
+            return;
+        }
+    printStar(n - 1);              
+    System.out.println("HelloWorld");      
+}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        printStar(n);
+    }
+}
