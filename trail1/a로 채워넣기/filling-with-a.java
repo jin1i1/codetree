f0a9b9d36f04c84396471f032cfaa11081726c8d
Hye Jin Lee <133628291+jin1i1@users.noreply.github.com>
2026-09-26 23:41:11 +0900

    [Passed] a로 채워넣기 (121ms, 10MB)

---
+++ trail1/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+                    String str = sc.next();
+
+                            char[] arr = str.toCharArray();
+
+                                    arr[1] = 'a';
+                                            arr[arr.length - 2] = 'a';
+
+                                                    System.out.print(new String(arr));
+                                                        }
+                                                        }
